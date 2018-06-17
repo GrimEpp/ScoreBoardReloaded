b799cdc201ba9206a833eb55cd9a79705aae7c2f
GrimEpp <GrimEpp@outlook.com>
2018-06-17 13:24:57 +0200

    Config fix

--- src/main/java/io/github/grimepp/scoreboard/commands/Reload.java
+++ src/main/java/io/github/grimepp/scoreboard/commands/Reload.java
@@ -1,19 +1,20 @@
 package io.github.grimepp.scoreboard.commands;
 
 import io.github.grimepp.scoreboard.ScoreboardPlugin;
+import io.github.grimepp.scoreboard.manager.ConfigAccess;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class Reload implements CommandExecutor {
+public class Reload implements CommandExecutor, ConfigAccess {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (!commandSender.hasPermission("admin.reload")) {
-            commandSender.sendMessage("&c&lFlame&b&lHeroes&8: &cDu har ikke tilgang!");
+        if (!commandSender.hasPermission((String) getConfig().get("settings.configreloadpermission"))) {
+            commandSender.sendMessage((String) getConfig().get("settings.noperm"));
             return true;
         }
         ScoreboardPlugin.getInstance().reloadConfig();
-        commandSender.sendMessage("Du reloaded config!");
+        commandSender.sendMessage((String) getConfig().get("settings.reloaded"));
         return true;
     }
 }
